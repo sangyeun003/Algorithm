b4a9d731e50b35c75e039f075695da58c9583da8
sangyeun003 <sangyeun003@gmail.com>
2024-02-15 01:04:30 +0900

    1629 ing

--- C+/Math/1629.c
+++ C+/Math/1629.c
@@ -1,1 +1,18 @@
 // modular 연산 활용!
+#include    <stdio.h>
+
+int main(void)
+{
+    long a;
+    long b;
+    long c;
+    long result;
+
+    scanf("%ld %ld %ld", &a, &b, &c);
+    result = 1;
+    a %= c;
+    for (int i = 0; i < b; i++)
+        result = result * a % c;
+    printf("%ld\n", result);
+    return (0);
+}
