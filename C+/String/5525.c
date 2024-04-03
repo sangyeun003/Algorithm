7248cea87ff7972d3fad8c7620659306114b4675
sangyeunpark <sangyeun003@gmail.com>
2024-04-03 19:14:24 +0900

    5525 ing

--- C+/String/5525.c
+++ C+/String/5525.c
@@ -1,18 +1,4 @@
 #include	<stdio.h>
-#include	<string.h>
-
-// 어긋나기 시작한 index를 return
-int	my_strncmp(char *s1, char *s2, int n)
-{
-	int	i;
-
-	for (i = 0; s1[i] && s2[i] && i < n; i++)
-	{
-		if (s1[i] != s2[i])
-			return (i);
-	}
-	return (0);
-}
 
 int	main(void)
 {
@@ -20,36 +6,21 @@ int	main(void)
 	int		m;
 	int		result;
 	char	s[1000001];
-	char	ioi[1000001];
-	int		diff;
+	int		start;
+	int		end;
 
 	scanf("%d", &n);
 	scanf("%d", &m);
 	scanf("%s", s);
 
-	for (int i = 0; i < 2 * n + 1; i++)
-		if (i % 2 == 0)
-			ioi[i] = 'I';
-		else
-			ioi[i] = 'O';
-	result = 0;
-	for (int i = 0; i + 2 * n + 1 - 1 < m; i++)
+	for (int i = 0; i < m; i++)
 	{
-		// printf("%d\n", strncmp(&s[i], ioi, 2 * n + 1));
-		if (s[i] == 'I' && s[i + 1] == 'O' && s[i + 2] == 'I')
+		if (s[i] == 'I')
 		{
-			diff = my_strncmp(s + i, ioi, 2 * n + 1);
-			if (diff == 0)
+			end = i + 2;
+			for (int j = 0; j < n; j++)
 			{
-				result++;
-				i++;
-			}
-			else
-			{
-				if (diff % 2 == 0)
-					i += diff;
-				else
-					i += diff - 1;
+				if ()
 			}
 		}
 	}
