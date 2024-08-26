5366dbd5eb80f0888a0101ec5d3d6dd9fc835739
sangyeunpark <sangyeun003@gmail.com>
2024-08-26 15:29:36 +0900

    4101

---
+++ ALOC/Implementation/4101.cpp
@@ -0,0 +1,21 @@
+#include	<iostream>
+
+using namespace std;
+
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	while (1)
+	{
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break ;
+		if (a > b)
+			cout << "Yes\n";
+		else
+			cout << "No\n";
+	}
+	return (0);
+}
